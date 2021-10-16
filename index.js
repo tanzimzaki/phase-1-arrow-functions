3bf8f47632326fe2c777598620ccf759d3ec077f
tanzimzaki <tanzimzaki@gmail.com>
2021-10-16 14:19:15 +0800

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+const a = 2000;
+const b = 100;
+const c = a / b;
+const z = 2;
+const num = z;
+
+const divide = function() {
+    return c;
+}
+
+const square = num =>  num * num;
+
+
+const add = (a, b) => a + b;
